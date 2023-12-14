e633089d3bf4f70142b47b9e4ea5f9807f8c3a5d
TecLegacy <kshvkumar.kk2@gmail.com>
2023-12-14 19:47:52 +0530

    first server setup

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -19,4 +21,18 @@ func main() {
 		log.Fatal("Port Not Found in ENV")
 	}
 	fmt.Printf("Port running on : %v", portString)
+
+	//CHI router
+	router := chi.NewRouter()
+
+	//Setup Server
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	srvErr := server.ListenAndServe()
+	if srvErr != nil {
+		log.Fatal("Something went wrong", srvErr)
+	}
 }
